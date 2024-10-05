30ea446e4c3dbe6842d437a5d6646ef790fc759d
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-10-05 16:48:31 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'pdf-cache-v1';
+const urlsToCache = [
+    // List any other files you want to cache here
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).then(fetchResponse => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
+});
