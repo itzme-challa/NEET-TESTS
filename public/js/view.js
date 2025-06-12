64966e96fecf4351195bd9420c6d454be9958af5
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 12:03:04 +0530

    Update view.js

--- public/js/view.js
+++ public/js/view.js
@@ -1,17 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, query, orderByChild, equalTo, get } from 'firebase/database';
-import { getDocument } from 'pdfjs-dist';
+import { firebaseConfig } from './config.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
+import { getDatabase, ref, query, orderByChild, equalTo, get } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js';
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const pdfjsLib = window['pdfjs-dist'];
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -143,7 +134,7 @@ function loadPDF() {
                     downloadButton.href = pdfUrl;
                     downloadButton.classList.remove('hidden');
 
-                    getDocument(pdfUrl).promise.then(pdf => {
+                    pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
                         pdfDoc = pdf;
                         pdfViewer.style.display = 'block';
                         downloadSection.style.display = 'none';
