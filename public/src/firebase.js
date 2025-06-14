7d3772a7464ca4612d0a3ad379f4db5211ecacd3
itzme-challa <challawaheed4@gmail.com>
2025-06-14 20:09:29 +0530

    Update firebase.js

--- public/src/firebase.js
+++ public/src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAolcB_o6f1CQPbLSYrMKTYaz_xYs54khY",
+    authDomain: "quizapp-1ae20.firebaseapp.com",
+    databaseURL: "https://quizapp-1ae20-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "quizapp-1ae20",
+    storageBucket: "quizapp-1ae20.appspot.com",
+    messagingSenderId: "626886802317",
+    appId: "1:626886802317:web:df08c307697ca235c45bc4",
+    measurementId: "G-NKJTC5C1XW"
 };
 
 const app = initializeApp(firebaseConfig);
